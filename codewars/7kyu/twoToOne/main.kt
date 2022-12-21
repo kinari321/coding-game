50f743bfe0f17f5e9a0a44fd5735a82e9ca599c4
kinari321 <abcdream321@yahoo.co.jp>
2022-12-22 00:05:05 +0900

    add other answer to twotoone

--- codewars/7kyu/twoToOne/main.kt
+++ codewars/7kyu/twoToOne/main.kt
@@ -1,3 +1,6 @@
+package twotoone
+
+// My Answer
 fun longest(s1:String, s2:String):String {
   val sb = StringBuilder()
   sb.append(s1)
@@ -5,4 +8,17 @@ fun longest(s1:String, s2:String):String {
   val chars: List<Char> = sb.toList()
   val noDuplicates = chars.toSet().toList()
   return noDuplicates.sorted().joinToString("")
+}
+
+// 1. Other Answer
+fun longest(s1:String, s2:String):String {
+  return (s1 + s2).toSortedSet().joinToString("")
+}
+
+// 2. Other Answer
+fun longest(s1:String, s2:String) = (s1 + s2).toSortedSet().joinToString("")
+
+// 3. Other Answer
+fun longest(s1:String, s2:String):String {
+  return "abcdefghijklmnopqrstuvwxyz".filter { it in s1 || it in s2 }
 }
