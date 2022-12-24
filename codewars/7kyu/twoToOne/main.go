9e141eb2ee596fc923b15d1ecb49d8fa1059fdcb
kinari321 <abcdream321@yahoo.co.jp>
2022-12-24 15:33:58 +0900

    add twoToOne other answer by language Go

--- codewars/7kyu/twoToOne/main.go
+++ codewars/7kyu/twoToOne/main.go
@@ -26,3 +26,28 @@ func TwoToOne(s1 string, s2 string) string {
 	}
 	return strings.Join(result, "")
 }
+
+// 1. Other Answer
+func TwoToOne1(s1 string, s2 string) string {
+	chars := strings.Split(s1+s2, "")
+	sort.Strings(chars)
+	result := ""
+	for _, s := range chars {
+		chr := string(s)
+		if !strings.Contains(result, chr) {
+			result = result + chr
+		}
+	}
+	return result
+}
+
+// 2. Other Answer
+func TwoToOne2(s1 string, s2 string) string {
+	result := ""
+	for _, char := range strings.Split("abcdefghijklmnopqrstuvwxyz", "") {
+		if strings.Contains(s1+s2, char) {
+			result += char
+		}
+	}
+	return result
+}
